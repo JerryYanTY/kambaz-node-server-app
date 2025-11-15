3f82505405466b42786d32e989bbd44028f16dd4
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-16 00:30:24 +0800

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
+import express from 'express';
+import Hello from './hello.js';
+import Lab5 from './Lab5/index.js';
 const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {
-    res.send('Welcome to full stack development!')
-})
+Lab5(app)
+Hello(app)
 app.listen(4000)
