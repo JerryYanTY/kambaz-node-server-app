11b86ed651fb6eea62ac18b62b11d766b48fff81
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-16 18:53:16 +0800

    5.2.3.4

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -6,6 +6,12 @@ const assignment = {
   completed: false,
   score: 0,
 };
+const module = {
+    id: "CS1-1",
+    name: "Intro to CS",
+    description: "Intro course to computer science.",
+    course: "CS1"
+};
 export default function WorkingWithObjects(app) {
   const getAssignment = (req, res) => {
     res.json(assignment);
@@ -13,6 +19,44 @@ export default function WorkingWithObjects(app) {
   const getAssignmentTitle = (req, res) => {
     res.json(assignment.title);
   };
+  const setAssignmentTitle = (req, res) => {
+    const { newTitle } = req.params;
+    assignment.title = newTitle;
+    res.json(assignment);
+  };
+  const setAssignmentScore = (req, res) => {
+    const { newScore } = req.params;
+    assignment.score = newScore;
+    res.json(assignment);
+  }
+  const setASsignmentComplete = (req, res) => {
+    const { newComp } = req.params;
+    assignment.completed = newComp;
+    res.json(assignment);
+  }
+  const getModule = (req, res) => {
+    res.json(module);
+  };
+  const getModuleName = (req, res) => {
+    res.json(module.name);
+  }
+  const setModuleName = (req, res) => {
+    const { newName } = req.params;
+    module.name = newName;
+    res.json(module);
+  };
+  const setModuleDescription = (req, res) => {
+    const { newDes } = req.params;
+    module.description = newDes;
+    res.json(module);
+  }
+  app.get("/lab5/module", getModule);
+  app.get("/lab5/module/name", getModuleName);
+  app.get("/lab5/module/name/:newName", setModuleName);
+  app.get("/lab5/module/description/:newDes", setModuleDescription);
+  app.get("/lab5/assignment/title/:newTitle", setAssignmentTitle);
   app.get("/lab5/assignment/title", getAssignmentTitle);
   app.get("/lab5/assignment", getAssignment);
+  app.get("/lab5/assignment/score/:newScore", setAssignmentScore);
+  app.get("/lab5/assignment/completed/:newComp", setASsignmentComplete);
 }
