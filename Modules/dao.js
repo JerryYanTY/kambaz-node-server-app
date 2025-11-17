d111d668ae076a8de00cff6045b921b903cd725b
JerryYanTY <yan.tianyu@northeastern.edu>
2025-11-17 19:48:49 +0800

    5.3.5.1

---
+++ Modules/dao.js
@@ -0,0 +1,10 @@
+export default function ModulesDao(db) {
+    function findModulesForCourse(courseId) {
+      const { modules } = db;
+      return modules.filter((module) => module.course === courseId);
+    }
+    return {
+      findModulesForCourse,
+    };
+   }
+   
